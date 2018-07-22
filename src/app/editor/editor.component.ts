e161427f1fe4b92c16770a6d16848bdedb4435ef
Unknown <lesommer.nathan@gmail.com>
2018-07-22 12:16:19 +0200

    Ajout de champs Titre et Auteur dans le mode edition

--- src/app/editor/editor.component.ts
+++ src/app/editor/editor.component.ts
@@ -12,8 +12,11 @@ export class EditorComponent implements OnInit {
   @ViewChild('textEditor') private _textEditor: CovalentTextEditorModule;
 
   storyContent:string = '';
-  storyTag:string = '';
-  hasNoTag:boolean = false;
+  storyAuthor:string = '';
+  hasNoAuthor:boolean = false;
+
+  storyTitle:string = '';
+  hasNoTitle:boolean = false;
 
   editorOptions: any = {
     placeholder : "C'est ici que commence l'histoire ...",
@@ -26,14 +29,16 @@ export class EditorComponent implements OnInit {
   }
 
   submitText() {
-    if(this.storyTag == ''){
-      alert("Mets un tag.");
-      this.hasNoTag = true;
-
+    if(this.storyAuthor == ''){
+      alert("Auteur requis.");
+      this.hasNoAuthor = true;
+    } else if(this.storyTitle == '') {
+      alert("Titre requis.");
+      this.hasNoTitle = true;
     } else {
       true;
       this.storyContent = this._textEditor["_value"];
-      let JSONStoreStory = "{ \"tag\": \""+this.storyTag+"\",\"content\":\""+this.storyContent+"\"}";
+      let JSONStoreStory = "{ \"title\" : \""+this.storyTitle +"\" \"author\": \""+this.storyAuthor+"\",\"content\":\""+this.storyContent+"\"}";
       console.log(JSONStoreStory);
       let JSONObject = JSON.parse(JSONStoreStory);
       console.log(JSONObject);
---
+++ src/app/story/story.ts
@@ -0,0 +1,8 @@
+export class Story {
+    path:string;
+    title:string;
+    author:string;
+    content:string;
+    active:string;
+    id:number;
+}
